e077a06fc3608f8a1ffc0a9a079fbf48379cc040
AnaBogatinovska <bogatinovska14@gmail.com>
2020-12-30 18:24:10 +0100

    update

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,57 +1,61 @@
 <template>
-  <div>
-     <Header />
-     <button class="add-item" @click="isOpenForm=true">add item</button>
-     <ShoppingList :shoppingItems="listItems"/>
-     <AddItemForm  v-if="isOpenForm" @cancelForm='isOpenForm = false' @onSubmitForm="onSubmit"/>
+  <div class="container">
+    <div class="inner-content">
+      <Header />
+      <button class="add-item" @click="isOpenForm = true">add item</button>
+      <ShoppingList :shoppingItems="listItems" />
+      <AddItemForm
+        v-if="isOpenForm"
+        @cancelForm="isOpenForm = false"
+        @onSubmitForm="onSubmit"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import AddItemForm from '../components/AddItemForm.vue'
-import Header from '../components/Header'
-import ShoppingList from '../components/ShoppingList.vue'
-import itemsStorage from '../storage/ListStore'
+import AddItemForm from "../components/AddItemForm.vue";
+import Header from "../components/Header";
+import ShoppingList from "../components/ShoppingList.vue";
+import itemsStorage from "../storage/ListStore";
 
 export default {
-    name: 'LandingPage',
-    components: {
-        Header,
-        ShoppingList,
-        AddItemForm
+  name: "LandingPage",
+  components: {
+    Header,
+    ShoppingList,
+    AddItemForm,
+  },
+  data() {
+    return {
+      isOpenForm: false,
+      listItems: [],
+    };
+  },
+  mounted() {
+    this.listItems = itemsStorage.getListItems();
+  },
+  methods: {
+    onSubmit(item) {
+      itemsStorage.pushItem(item);
+      this.isOpenForm = false;
     },
-    data() {
-      return {
-        isOpenForm:false,
-        listItems: [],
-      }
+    test() {
+      console.log("in");
+      this.isOpenCart = true;
     },
-    mounted() {
-      this.listItems= itemsStorage.getListItems();
-    },
-    methods: {
-      onSubmit(item) {
-        itemsStorage.pushItem(item)
-        this.isOpenForm = false
-      },
-      test() {
-        console.log('in')
-        this.isOpenCart = true
-      }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 .add-item {
   margin: 20px 5px;
-    background: #e88635;
-    color: #fff;
-    border: none;
-    text-transform: uppercase;
-    border-radius: 10px;
-    padding: 10px 10px;
-
+  background: #e88635;
+  color: #fff;
+  border: none;
+  text-transform: uppercase;
+  border-radius: 10px;
+  padding: 10px 10px;
 }
-
 </style>
